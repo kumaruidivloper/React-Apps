d71867254b49611c8b6599a739d5158e619ea1f2
kumaruidivloper <kumaruidivloper@gmail.com>
2025-06-19 18:59:21 +1000

    context-API-Approch

--- src/ContextExample.jsx
+++ src/ContextExample.jsx
@@ -1,50 +1,52 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
+
+let UserContext = createContext()
 
 const Component1 = () => {
     const [user, setUser] = useState('Kumar')
 
     return (
-        <>
+        <UserContext.Provider value={user}>
         <h3>Component 1</h3>
         <h1>{`Hello From Component 1 ${user}`}</h1>
-        <Component2 user={user}/>
-        </>
+        <Component2/>
+        </UserContext.Provider>
     )
 }
 
-const Component2 = ({user}) => {
+const Component2 = () => {
 
 
     return (
         <>
         <h3>Component 2</h3>
-        <Component3 user={user}/>
+        <Component3/>
         </>
     )
 }
 
-const Component3 = ({user}) => {
+const Component3 = () => {
 
     return (
         <>
         <h3>Component 3</h3>
-        <Component4 user={user}/>
+        <Component4/>
         </>
     )
 }
 
-const Component4 = ({user}) => {
+const Component4 = () => {
 
     return (
         <>
         <h3>Component 4</h3>
-        <Component5 user={user}/>
+        <Component5/>
         </>
     )
 }
 
-const Component5 = ({user}) => {
-
+const Component5 = () => {
+    let user = useContext(UserContext)
     return (
         <>
         <h3>Component 5</h3>
