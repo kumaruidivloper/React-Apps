fd016c6879a43ab7f817a55c88f90569fd2818b0
kumaruidivloper <kumaruidivloper@gmail.com>
2025-06-20 07:17:40 +1000

    feature/chid-To-Parent-Counter-pulse-Minus

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 import CounterButton from './child';
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(10);
 
   const increaseCounter = () => {
     setCount(prev => prev + 1);
+  };
+
+  const decreaseCounter = () => {
+    setCount(prev => prev - 1);
   };
 
   return (
@@ -14,7 +18,7 @@ const App = () => {
       <p>Counter: {count}</p>
 
       {/* Pass the function to child */}
-      <CounterButton onIncrease={increaseCounter} />
+      <CounterButton onDecrease={decreaseCounter} onIncrease={increaseCounter} />
     </div>
   );
 };
--- src/child.jsx
+++ src/child.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const CounterButton = ({ onIncrease }) => {
+const CounterButton = ({ onIncrease, onDecrease }) => {
   return (
     <div>
       <h2>Child Component</h2>
       <button onClick={onIncrease}>Increase Counter</button>
+      <button onClick={onDecrease}>Increase Counter</button>
     </div>
   );
 };
